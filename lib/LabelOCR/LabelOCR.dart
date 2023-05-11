4aba1e195b6691f6e451b10e8e8927487283eaf0
Seibell <66213395+Seibell@users.noreply.github.com>
2023-05-11 16:57:11 +0800

    added spinny wheel when loading extracted text

--- lib/LabelOCR/LabelOCR.dart
+++ lib/LabelOCR/LabelOCR.dart
@@ -13,6 +13,7 @@ class LabelOCR extends StatefulWidget {
 class _LabelOCRState extends State<LabelOCR> {
   final ValueNotifier<String> _extractedTextNotifier =
       ValueNotifier<String>('');
+  bool _isLoading = false; // Added variable to track loading state
 
   Future<void> saveExtractedText(String text) async {
     String timestamp = DateTime.now().toString();
@@ -22,26 +23,49 @@ class _LabelOCRState extends State<LabelOCR> {
     };
     int id = await DatabaseHelper.instance.insert(row);
 
-    //To be removed (for testing purposes only)
+    // To be removed (for testing purposes only)
     print("Inserted row with ID: $id");
   }
 
+  Future<File> applyPreprocessing(String imagePath) async {
+    img.Image image = img.decodeImage(File(imagePath).readAsBytesSync())!;
+
+    // To be removed soon (for testing only)
+    print("original width = ${image.width}");
+    print("original height = ${image.height}");
+
+    int newWidth = (image.width * 1.23).round();
+    int newHeight = (image.height * 1.5).round();
+
+    // Resize the image - optimal text height for OCR is 20-32 pixels
+    image = img.copyResize(image, height: newHeight, width: newWidth);
+    // Apply Gaussian blur to the image
+    // image = img.gaussianBlur(image, radius: 10);
+
+    // Apply preprocessing to increase the contrast
+    image = img.adjustColor(image, contrast: 2.69);
+
+    // Convert the image to grayscale (optional)
+    image = img.grayscale(image);
+
+    // Convert the processed image back to a file
+    final List<int> processedImageBytes = img.encodeJpg(image);
+    final processedImageFile = File(imagePath)
+      ..writeAsBytesSync(processedImageBytes);
+
+    return processedImageFile;
+  }
+
   Future<void> _readTextFromImage(ImageSource source) async {
+    setState(() {
+      _isLoading = true; // Show loading spinner
+    });
+
     final ImagePicker _picker = ImagePicker();
     final XFile? pickedFile = await _picker.pickImage(source: source);
 
     if (pickedFile != null) {
-      // Load the image file
-      img.Image image =
-          img.decodeImage(File(pickedFile.path).readAsBytesSync())!;
-
-      // Apply preprocessing to increase the contrast
-      image = img.adjustColor(image, contrast: 2.0);
-
-      // Convert the processed image back to a file
-      final List<int> processedImageBytes = img.encodeJpg(image);
-      final processedImageFile = File(pickedFile.path)
-        ..writeAsBytesSync(processedImageBytes);
+      final processedImageFile = await applyPreprocessing(pickedFile.path);
 
       // Create an InputImage instance from the processed image file
       final inputImage = InputImage.fromFilePath(processedImageFile.path);
@@ -55,6 +79,10 @@ class _LabelOCRState extends State<LabelOCR> {
 
       textRecognizer.close();
     }
+
+    setState(() {
+      _isLoading = false; // Hide loading spinner
+    });
   }
 
   @override
@@ -83,14 +111,18 @@ class _LabelOCRState extends State<LabelOCR> {
         ValueListenableBuilder<String>(
           valueListenable: _extractedTextNotifier,
           builder: (context, value, child) {
-            return Expanded(
-              child: SingleChildScrollView(
-                child: Text(
-                  value,
-                  style: const TextStyle(fontSize: 16),
+            if (_isLoading) {
+              return CircularProgressIndicator(); // Show loading spinner
+            } else {
+              return Expanded(
+                child: SingleChildScrollView(
+                  child: Text(
+                    value,
+                    style: const TextStyle(fontSize: 16),
+                  ),
                 ),
-              ),
-            );
+              );
+            }
           },
         ),
       ],
