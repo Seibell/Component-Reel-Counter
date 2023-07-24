b091af0531f961f3b09d88ec811bcea9459f212c
Seibell <66213395+Seibell@users.noreply.github.com>
2023-07-24 17:12:13 +0800

    update: added support for data appearing in line below for OCR (for cases without ':'

--- lib/LabelOCR/text_bucketing.dart
+++ lib/LabelOCR/text_bucketing.dart
@@ -22,6 +22,10 @@ class TextBucketing {
         "Mfr. P/N",
         "OUR P/N",
         "Our P/N",
+        "Part Number",
+        "Mfg P/N",
+        "Manufacturer Product Number",
+        "Manufacturer P/N"
       ],
       "Customer Product Number": [
         "Customer Product Number",
@@ -44,39 +48,81 @@ class TextBucketing {
         "QTY: PCS",
         "Qty: pcs"
       ],
+      "Description": [
+        "Desc",
+        "Description",
+        "DESC",
+        "Part Description",
+        "Part Desc",
+        "Part Desc.",
+        "Part Description.",
+        "PART DESCRIPTION",
+        "DESCRIPTION"
+      ]
     };
 
     Map<String, List<String>> results = {"Uncategorized": []};
     bool categorized;
 
     // Iterate over the tokens
-    for (String token in tokens) {
+    for (int i = 0; i < tokens.length; i++) {
+      String token = tokens[i];
       categorized = false;
 
-      // Iterate over the targets
-      for (String target in targets.keys) {
-        // Find the closest match for each variation of the current target
-        for (String variation in targets[target]!) {
-          ExtractedResult<String> closestMatch =
-              fuzzy.extractOne(query: variation, choices: [token]);
+      // If this token is a potential label, check the next token as well
+      if (i + 1 < tokens.length && !token.contains(':')) {
+        String nextToken = tokens[i + 1];
 
-          if (closestMatch.score >= 70) {
-            if (!results.containsKey(target)) {
-              results[target] = [];
+        // Iterate over the targets
+        for (String target in targets.keys) {
+          // Find the closest match for each variation of the current target
+          for (String variation in targets[target]!) {
+            ExtractedResult<String> closestMatch =
+                fuzzy.extractOne(query: variation, choices: [token]);
+
+            if (closestMatch.score >= 70) {
+              if (!results.containsKey(target)) {
+                results[target] = [];
+              }
+              results[target]!.add(nextToken);
+              categorized = true;
+              i++; // Skip the next token, as we've already categorized it
+              break;
             }
-            results[target]!.add(token.replaceFirst(variation, '').trim());
-            categorized = true;
+          }
+
+          if (categorized) {
             break;
           }
-        }
-
-        if (categorized) {
-          break;
         }
       }
 
       if (!categorized) {
-        results["Uncategorized"]!.add(token);
+        // The original code: categorize the token itself, not the next token
+        for (String target in targets.keys) {
+          // Find the closest match for each variation of the current target
+          for (String variation in targets[target]!) {
+            ExtractedResult<String> closestMatch =
+                fuzzy.extractOne(query: variation, choices: [token]);
+
+            if (closestMatch.score >= 70) {
+              if (!results.containsKey(target)) {
+                results[target] = [];
+              }
+              results[target]!.add(token.replaceFirst(variation, '').trim());
+              categorized = true;
+              break;
+            }
+          }
+
+          if (categorized) {
+            break;
+          }
+        }
+
+        if (!categorized) {
+          results["Uncategorized"]!.add(token);
+        }
       }
     }
 
